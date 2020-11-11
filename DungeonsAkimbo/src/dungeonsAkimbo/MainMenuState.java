4b342f6b806d10d4d3c40dda0008edefe46d5790
Christopher Williams <cwill3890@gmail.com>
2020-11-10 16:32:05 -0800

    Initial gamestates and gamelogic created.

---
+++ DungeonsAkimbo/src/dungeonsAkimbo/MainMenuState.java
@@ -0,0 +1,60 @@
+package dungeonsAkimbo;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.GameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class MainMenuState extends BasicGameState {
+
+	
+
+
+	@Override
+	public void init(GameContainer container, StateBasedGame game) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void enter(GameContainer container, StateBasedGame game) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
+		// TODO Auto-generated method stub
+		DungeonsAkimboGame dag = (DungeonsAkimboGame)game;
+		
+		g.drawString("press space to begin", dag.screenWidth/2, dag.screenHeight/2);
+	}
+
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		// TODO Auto-generated method stub
+		Input input = container.getInput();
+		
+		if(input.isKeyDown(Input.KEY_SPACE)) {
+			game.enterState(DungeonsAkimboGame.PLAYINGSTATE);
+		}
+	}
+
+
+	@Override
+	public void leave(GameContainer container, StateBasedGame game) throws SlickException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return DungeonsAkimboGame.MAINMENUSTATE;
+	}
+
+
+}
