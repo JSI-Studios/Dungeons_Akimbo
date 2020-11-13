6a13570dfa669a308ddaf12c44c29281c4c5c6c7
Christopher Williams <cwill3890@gmail.com>
2020-11-12 16:45:17 -0800

    Early camera implementation started.

---
+++ DungeonsAkimbo/src/dungeonsAkimbo/DaCamera.java
@@ -0,0 +1,58 @@
+package dungeonsAkimbo;
+
+public class DaCamera {
+	
+	private float xOffSet; //offset of the camera in the x
+	private float yOffSet; //offset of the camera in the y
+	private int tileWidth; //tile width in px
+	private int tileHeight; // tileHeight in px
+	private int mapHeight; //Map height in tiles
+	private int mapWidth;	//Map width in tiles
+	
+	DaMap currentGame; //current game's map;
+	
+	public boolean Debug;
+	
+	public DaCamera(DaMap map) {
+		super();
+		xOffSet = 0;
+		yOffSet = 0;
+		
+		currentGame = map;
+		
+		tileWidth = currentGame.getTileWidth();
+		tileHeight = currentGame.getTileHeight();
+		mapHeight = currentGame.getHeightInTiles();
+		mapWidth = currentGame.getWidthInTiles();
+		
+		
+	}
+	
+	public float getCameraX() {
+		return xOffSet;
+	}
+	
+	public float getCameraY() {
+		return yOffSet;
+	}
+	
+	public void moveCameraX(int dx, int delta) {
+		if (dx > 0 && xOffSet < mapWidth - 16) {
+			xOffSet += .00195f * delta;
+		}else if(dx < 0 && xOffSet > 0){
+			xOffSet -= .00195f * delta;
+		}
+		return;
+		
+	}
+	
+	public void moveCameraY(int dy, int delta) {
+		if (dy > 0 && yOffSet < mapHeight - 16) {
+			yOffSet += .00195f * delta;
+		}else if(dy < 0 && yOffSet > 0) {
+			yOffSet -= .00195f * delta;
+		}	
+		return;		
+	}
+
+}
