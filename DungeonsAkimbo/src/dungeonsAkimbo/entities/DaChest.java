d66a03c9bb3e1e9e12f2677a344da12a6a46c373
Christopher Williams <cwill3890@gmail.com>
2020-12-15 00:24:08 -0800

    added content groups to chests.

--- DungeonsAkimbo/src/dungeonsAkimbo/entities/DaChest.java
+++ DungeonsAkimbo/src/dungeonsAkimbo/entities/DaChest.java
@@ -4,12 +4,12 @@ import jig.Entity;
 import jig.Vector;
 
 public class DaChest extends Entity {
-	private int contents;
+	private int contentsGroup;
 	private boolean state;
 	
 	public DaChest(float px, float py, int contents) {
 		this.setPosition(new Vector(px, py));
-		this.contents = contents;
+		this.contentsGroup = contents;
 		this.state = false;
 		
 		//todo: rig up open/close sprites
@@ -20,7 +20,7 @@ public class DaChest extends Entity {
 		int[] contents = null;
 		
 		if (!this.state) {
-			contents = getContents(this.contents);
+			contents = getContents(this.contentsGroup);
 			this.state = true;
 			
 			}
@@ -29,8 +29,20 @@ public class DaChest extends Entity {
 	}
 
 
-	private int[] getContents(int contents2) {
+	private int[] getContents(int contentsGroup) {
 		// TODO return an array with ints representing objects ie. 1=health 2=ammo 3=powerup 4=gold bag 5=gold pieces
+		if(contentsGroup == 0) {
+			return new int[] {1,1,2};
+		}else if (contentsGroup == 1) {
+			return new int[] {1,2,2};
+		}else if (contentsGroup == 2) {
+			return new int[] {2,2};
+		}else if (contentsGroup == 3) {
+			return new int[] {3,5,5};
+		}else if (contentsGroup == 4) {
+			return new int[] {5,4,4};
+		}
+		
 		return null;
 	}
 }
