792113841b1fbe00773f431beda202f674897097
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-12-15 20:30:04 -0800

    Begin structure for final boss

---
+++ DungeonsAkimbo/src/dungeonsAkimbo/entities/DaBoi.java
@@ -0,0 +1,19 @@
+package dungeonsAkimbo.entities;
+
+import jig.Entity;
+
+public class DaBoi extends Entity implements DaEnemy {
+
+	@Override
+	public void collisionAction(boolean isHit, boolean isPlayer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Projectile attack(Entity player) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
