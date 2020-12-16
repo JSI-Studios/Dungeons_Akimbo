36f355988cf113831eeb3c60c85a12c0c8645627
Christopher Williams <cwill3890@gmail.com>
2020-12-16 01:33:29 -0800

    improve door code

--- DungeonsAkimbo/src/dungeonsAkimbo/entities/DaDoor.java
+++ DungeonsAkimbo/src/dungeonsAkimbo/entities/DaDoor.java
@@ -5,23 +5,35 @@ import jig.Vector;
 
 public class DaDoor extends Entity{
 	
-	private int doorId;
+	private int doorId, direction;
 	private boolean state;
 	
-	public DaDoor(float px, float py, boolean state, int doorId) {
+	public DaDoor(float px, float py, boolean state, int doorId, int direction) {
 		this.doorId = doorId;
 		this.state = state;
+		this.direction = direction; //0=N 1=W 2=S 3=E
 		this.setPosition(new Vector(px, py));
 		
 		//todo: pick sprite and setup vertical vs horizontal
+		
 	}
 	
 	
 	public void activate() {
 		if(!state) {
-			//open the door
+			if(direction == 1 || direction == 3) {
+				this.translate(new Vector(0,32));
+			}else {
+				this.translate(new Vector(32,0));
+			}
+			state = !state;
 		}else {
-			//close the door
+			if(direction == 1 || direction == 3) {
+				this.translate(new Vector(0,-32));
+			}else {
+				this.translate(new Vector(-32,0));
+			}
+			state = !state;
 		}
 	}
 	
