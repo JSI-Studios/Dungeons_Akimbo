9834a26924dd4e766badb7332d53b68d22f0ebca
MinhQuan Tran <minhquan.tran@wsu.edu>
2020-12-16 07:54:01 -0800

    Added camera shifting for multiplayer

--- DungeonsAkimbo/src/dungeonsAkimbo/states/MultiPlayTestState.java
+++ DungeonsAkimbo/src/dungeonsAkimbo/states/MultiPlayTestState.java
@@ -3,6 +3,7 @@ package dungeonsAkimbo.states;
 import dungeonsAkimbo.DungeonsAkimboGame;
 import dungeonsAkimbo.InputListeners.DaJoyconListener;
 import dungeonsAkimbo.entities.DaAssault;
+import dungeonsAkimbo.entities.Player;
 import dungeonsAkimbo.entities.Projectile;
 import dungeonsAkimbo.gui.ChatGUI;
 import dungeonsAkimbo.gui.DaCamera;
@@ -14,6 +15,7 @@ import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MultiPlayTestState extends BasicGameState {
@@ -30,6 +32,8 @@ public class MultiPlayTestState extends BasicGameState {
     
 	private Sound currentSound;
 
+	private int[] cameraDirection;
+	
     @Override
     public void init(GameContainer container, StateBasedGame game) throws SlickException {
         // TODO Auto-generated method stub
@@ -60,6 +64,7 @@ public class MultiPlayTestState extends BasicGameState {
 		container.setSoundOn(true);
 		currentSound = ResourceManager.getSound(DungeonsAkimboGame.BGM);
 		currentSound.loop(1f, 0.2f);
+		cameraDirection = new int[]{0, 0, 0, 0};
 
     }
 
@@ -224,7 +229,46 @@ public class MultiPlayTestState extends BasicGameState {
             if (dag.getCurrentMap().getPlayerList().get(playerID).getPrimaryWeapon() instanceof DaAssault) {
                 dag.getLogic().assaultBurst(dag, playerID, shotAngle);
             }
+            
+
+        	// Handle camera movement based on number of players
+            Player player = dag.getCurrentMap().getPlayerList().get(playerID);
+            if(player.getVelocity().length() > 0) {
+	            if(player.getY() - (gameView.getCameraY() * 64) < 512 && gameView.getCameraY() > 0) {
+	            		this.cameraDirection[0]++;
+	            }
+	            if(player.getY() - (gameView.getCameraY() * 64) > 512 && gameView.getCameraY() < dag.getCurrentMap().getHeightInTiles()) {
+	            		this.cameraDirection[1]++;
+	            }
+	            if(player.getX() - (gameView.getCameraX() * 64) < 512 && gameView.getCameraX() > 0) {
+	            		this.cameraDirection[2]++;
+	            }
+	            if(player.getX() - (gameView.getCameraX() * 64) > 512 && gameView.getCameraX() < dag.getCurrentMap().getWidthInTiles()) {
+	            		this.cameraDirection[3]++;
+	            }
+            }
+            
         }
+        
+        /* Handle camera movement */
+        
+        //  Handle y axis
+        if(cameraDirection[0] == 1) {
+        	gameView.moveCameraY(-1, delta);
+        }
+        if(cameraDirection[1] == 1) {
+        	gameView.moveCameraY(1, delta);
+        }
+        // Handle x axis
+        if(cameraDirection[2] == 1) {
+        	gameView.moveCameraX(-1, delta);
+        }
+        if(cameraDirection[3] == 1) {
+        	gameView.moveCameraX(1, delta);
+        }
+        // Reset camera direction
+        this.cameraDirection = new int[] {0, 0, 0, 0};
+        
         //update Logic
         dag.getLogic().localUpdate(delta);
         updateChatLog(dag);
